f798604d22b315cef4f9581724b45905715818c9
Clay Dunston <dunstontc@gmail.com>
2018-02-02 13:35:42 -0600

    Removed extra using statements in TacoBell.cs

--- LoggingKata/TacoBell.cs
+++ LoggingKata/TacoBell.cs
@@ -1,6 +1,4 @@
-﻿using log4net.Core;
-
-namespace LoggingKata
+﻿namespace LoggingKata
 {
     public class TacoBell : ITrackable
     {
@@ -9,14 +7,10 @@ namespace LoggingKata
         {
             Name = name;
             Location = new Point(lon, lat);
-            Lon = lon;
-            Lat = lat;
         }
         
         public string Name { get; set; }
         public Point Location { get; set; }
-        public double Lat;
-        public double Lon;
 
     }
 }
