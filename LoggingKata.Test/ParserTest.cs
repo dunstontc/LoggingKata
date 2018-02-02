d5152fb5ad29593100504c86e0b90b45ce35fc15
Clay Dunston <dunstontc@gmail.com>
2018-02-02 12:37:48 -0600

    Added initial tests

--- LoggingKata.Test/ParserTest.cs
+++ LoggingKata.Test/ParserTest.cs
@@ -11,9 +11,28 @@ namespace LoggingKata.Test
     public class TacoParserTestFixture
     {
         [Test]
+        public void ShouldReturnNullForEmptyString()
+        {
+            var testNull =  "";
+            var parser = new TacoParser();
+
+            var result = parser.Parse(testNull);
+
+            Assert.Null(result);
+        }
+
+        [Test]
         public void ShouldParseLine()
         {
-            //TODO: Complete ShouldParseLine
+            var tacoParser = new TacoParser();
+            var tacoCoordinates =
+                "-84.677017, 34.073638,Taco Bell Acwort... (Free trial * Add to Cart for a full POI info) ";
+
+            var resultTaco = tacoParser.Parse(tacoCoordinates);
+
+            Assert.IsNotNull(resultTaco);
         }
     }
 }
+
+
